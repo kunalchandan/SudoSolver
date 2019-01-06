746c2b852c54cc0b50499f873f0d7f90c30ea688
Kunal Chandan <kunal_chandan@outlook.com>
2019-01-05 21:20:53 -0500

    Forgot to add this to previous commit

--- src/kunal/chandan/Main.java
+++ src/kunal/chandan/Main.java
@@ -6,7 +6,11 @@ public class Main {
 
     public static void main(String[] args) throws IOException, InterruptedException {
         Sudoku su = new Sudoku();
-        su.readGrid("src/kunal/chandan/sudo.txt");
-        su.solve();
+        su.readGrid("src/kunal/chandan/sudoHard.txt");
+        long count = su.solve();
+        System.out.println("** ** ** ** ** ** ** Solved Final State ** ** ** ** ** ** **");
+        System.out.println("\nAnalytics posted here:");
+        System.out.println("Number of times cells were filled " + count);
+        su.printGrid();
     }
 }
